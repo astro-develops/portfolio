2fbc5908d35b218732b05c9bfc2878edbbbaa7c4
Astro <114089666+astro-develops@users.noreply.github.com>
2024-06-19 13:27:32 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-24 text-[#CCC] ml-72 bg-[url('/a.svg')] bg-no-repeat bg-cover bg-center">
       <h1 className="text-6xl font-bold mt-24 font-bold">
-        Hi, I'm <b className="bg-clip-text text-transparent bg-gradient-to-r from-[#49B2C4] to-[#2745C8]">Astro</b>
+        Hi, I&apos;m <b className="bg-clip-text text-transparent bg-gradient-to-r from-[#49B2C4] to-[#2745C8]">Astro</b>
       </h1>
       <p className="text-[#CCC] my-4">16 year old aspiring full stack web developer</p>
       <div className="flex flex-row gap-4">
